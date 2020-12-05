c306b04ffe0f5130f4ac7b3593523198de095b2a
gruv19 <grv1992@yandex.ru>
2020-12-05 22:59:35 +0500

    added header

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,8 @@ const cssmin = require('gulp-cssmin');
 
 gulp.task('sass', function() {
   return gulp.src('app/scss/**/*.scss')
-              .pipe(sass({outputStyle: 'compressed'}))
+              /*.pipe(sass({outputStyle: 'compressed'}))*/
+              .pipe(sass({outputStyle: 'expanded'}))
               .pipe(rename({suffix: '.min'}))
               .pipe(autoprefixer({
                 overrideBrowserslist: ['last 8 versions']
@@ -58,9 +59,9 @@ gulp.task('browser-sync', function() {
 });
 
 gulp.task('watch', function() {
-  gulp.watch('app/scss/style.scss', gulp.parallel('sass'));
+  gulp.watch('app/scss/**/*.scss', gulp.parallel('sass'));
   gulp.watch('app/*.html', gulp.parallel('html'));
   gulp.watch('app/js/*.js', gulp.parallel('js'));
 });
 
-gulp.task('default', gulp.parallel(/*'script', */'sass', 'watch', 'browser-sync'));
+gulp.task('default', gulp.parallel(/*'script', 'sass', */'watch', 'browser-sync'));
